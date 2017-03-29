839c89c5efe20ab30352f6df07ce2456201b5b74
Daniel Havir <havir.daniel@gmail.com>
2017-03-29 15:19:02 +0200

    more providers added

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Run the script with a command:
 
 ```python cdn.py `url here` ```
+
+Example:
+
+```python cdn.py bbc.com```
--- cdn.py
+++ cdn.py
@@ -1,3 +1,23 @@
+def adjust(url):
+	"""Adjust urls extracted using regex, correct for extraction errors.
+	e.g. ://ssl.bbc.co.uk\' : \'http://www.bbc.co.uk\')+\'/wwscripts/flag" """
+	if url.find("//") != -1:
+		url = url[url.find("//")+2:]
+	if url.find("/") != -1:
+		url = url[:url.find("/")]
+	if url.find("?") != -1:
+		url = url[:url.find("?")]
+	if url.find(" ") != -1:
+		url = url[:url.find(" ")]
+	if url.find("'") != -1:
+		url = url[:url.find("'")]
+	if url.find('"') != -1:
+		url = url[:url.find('"')]
+	if url.startswith("www"):
+		url = url[4:]
+
+	return url
+
 def dig_cdn(url):
 	import dns.resolver
 	import requests
@@ -6,34 +26,24 @@ def dig_cdn(url):
 	try:
 		if not url.startswith('http'):
 			url = "http://" + url
-		html = requests.get(url).text
+		r = requests.get(url)
 	except:
 		return "Invalid url"
 	
-	name = url[:url.find(".")]
+	html = r.text
+	name = adjust(url)
+	name = name[:name.find(".")]
 
-	link_re = re.compile('://.*?/')
+	link_re = re.compile('//.*?/')
 	links = link_re.findall(html)
-	
-	def adjust(url):
-		"""Adjust urls extracted using regex, correct for extraction errors.
-		e.g. ://ssl.bbc.co.uk\' : \'http://www.bbc.co.uk\')+\'/wwscripts/flag" """
-		if url.startswith("://"):
-			url = url[3:]
-		if url.find("/") != -1:
-			url = url[:url.find("/")]
-		if url.find("?") != -1:
-			url = url[:url.find("?")]
-		if url.find(" ") != -1:
-			url = url[:url.find(" ")]
-		if url.find("'") != -1:
-			url = url[:url.find("'")]
-		if url.find('"') != -1:
-			url = url[:url.find('"')]
-		if url.startswith("www"):
-			url = url[4:]
-		
-		return url
+
+	def query_dns(url):
+		try:
+			answer = dns.resolver.query(url).rrset.to_text()
+		except:
+			answer = ""
+
+		return answer
 
 	def cdn_lookup(answer):
 		#Look for CDN provider within a url or a DNS response.
@@ -46,43 +56,28 @@ def dig_cdn(url):
 		if "cdn77" in answer:
 			cdn += "CDN77 "
 		if "netdna" in answer:
-			cdn += "MaxCDN "
+			cdn += "MaxCDN/StackPath "
+		if "hwcdn" in answer:
+			cdn += "Highwinds/StackPath "
 		if "fastly" in answer:
 			cdn += "Fastly "
 		if "cdngc" in answer:
 			cdn += "CDNetworks "
+		if "cloudflare" in answer:
+			cdn += "Cloudflare "
 
 		return cdn.rstrip()
 
-	def query_dns(url):
-		try:
-			answer = dns.resolver.query(url).rrset.to_text()
-		except:
-			answer = ""
-
-		return answer
-
 	links = map(adjust, links)
 
-	#Count the frequency of links on a page
-	link_set = {}
+	#Iterate through all links found on the main page and filter for links with domain name
+	link_set = set()
 	for url in links:
 		if name in url or cdn_lookup(url):
-			if url in link_set:
-				link_set[url] += 1
-			else:
-				link_set[url] = 1
-
-	#Filter links that appears more than once
-	links = []
-	for url, frequency in link_set.items():
-		if frequency > 1 and re.search("([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?", url):
-			links.append(url)
-
-	link_set = None
+			link_set.add(url)
 
 	#Query for DNS records. Join list to a single string.
-	answer = "\n".join(list(map(query_dns,links)))
+	answer = "\n".join(list(map(query_dns,link_set)))
 
 	return cdn_lookup(answer)
 
@@ -90,5 +85,5 @@ if __name__ == '__main__':
 	import sys
 	output = dig_cdn(sys.argv[1])
 	if output == "":
-		output = "No CDN provider was found."
+		output = "No CDN Provider found."
 	print output
